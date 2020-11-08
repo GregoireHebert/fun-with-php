ffc755a13922f0f9b85aa36b1653321072bbafa4
Sofia Novoa <sofia.novoa.etu@univ-lille.fr>
2020-11-08 19:00:05 +0100

    Use bcmath librairyfor operations

--- src/Entity/Level.php
+++ src/Entity/Level.php
@@ -4,7 +4,9 @@
 class Level
 {
     public function newLevel(Player $player ,$res, $proba) {
-        return $player->getPoints() + 32 * ($res - $proba);
+        $substract = bcsub($res, $proba, 5);
+        $mul = bcmul(32, $substract, 5);
+        return bcadd($player->getPoints(), $mul, 5);
 
     }
 }
--- src/Entity/Probability.php
+++ src/Entity/Probability.php
@@ -5,6 +5,8 @@ class Probability
 {
 
     public function getProbability(Player $playerA, Player $playerB) {
-        return 1 / (1 + (pow(10,($playerB->getPoints() - $playerA->getPoints()) /400)));
+        $sub = bcsub($playerB->getPoints(),$playerA->getPoints());
+        $exponent = bcdiv($sub, 400, 10);
+        return bcdiv(1, bcadd(1, pow(10, $exponent), 10), 10);
     }
 }
