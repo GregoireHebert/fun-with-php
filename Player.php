f27e71b89153e2e4964da33cd8982cb77934bd3d
mvkopp <mael.avennec@hotmail.fr>
2020-11-09 02:23:59 +0100

    Update Player.php

--- Player.php
+++ Player.php
@@ -2,18 +2,20 @@
 
 class Player{
 
+    private const DEFAULT_POINTS = 1200;
     private $points=1200;
+    private $name=null;
 
-    public function __construct(){
-        $this->points=1200;
+    public function __construct($name){
+        $this->points=self::DEFAULT_POINTS;
+        $this->name=$name;
     }
 
-    public function getPoints(){
+    public function getPoints():int{
         return $this->points;
     }
 
-    public function setPoints($points){
+    public function setPoints($points):void{
         $this->points=$points;
     }
-
 }
