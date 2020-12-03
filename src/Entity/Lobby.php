ec606efb7829eadf01de31351a4145d454eb52ea
Legris <legristhomaspro@gmail.com>
2020-12-03 12:16:00 +0100

    creation entity/controller Lobby

---
+++ src/Entity/Lobby.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\LobbyRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=LobbyRepository::class)
+ */
+class Lobby
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="array", nullable=true)
+     */
+    private $players = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPlayers(): ?array
+    {
+        return $this->players;
+    }
+
+    public function setPlayers(?array $players): self
+    {
+        $this->players = $players;
+
+        return $this;
+    }
+}
