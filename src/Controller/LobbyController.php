43ae68514cf747f7e3b4b34aedef027e629e1c9e
mvkopp <mael.avennec@hotmail.fr>
2020-12-03 12:47:39 +0100

    inscription d'un joueur dans le lobby

--- src/Controller/LobbyController.php
+++ src/Controller/LobbyController.php
@@ -2,7 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Lobby;
+use App\Form\LobbyType;
+use App\Repository\LobbyRepository;
+use App\Entity\Player;
+use App\Form\PlayerType;
+use App\Repository\PlayerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
@@ -18,5 +25,29 @@ class LobbyController extends AbstractController
         return $this->render('lobby/index.html.twig', [
             'controller_name' => 'LobbyController',
         ]);
+    }
+
+    /**
+     * @Route("/lobby/register/{id}", name="lobby_rehister", methods={"POST"})
+     * @Security("is_granted('ROLE_USER')")
+     */
+    public function register(Request $request): Response
+    {
+        $lobby = new Lobby();
+        $form = $this->createForm(LobbyType::class, $lobby);
+        $form->handleRequest($request);
+
+        $id=$request->get('id');
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $lobby->setPlayerId($id);
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($lobby);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('lobby');
+        }
+
+        return $this->redirectToRoute('lobby');
     }
 }
