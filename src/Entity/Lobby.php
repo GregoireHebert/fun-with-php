63d79951655c517bb123e0da18cc5e122da0dd96
mvkopp <mael.avennec@hotmail.fr>
2020-12-03 12:14:51 +0100

    create lobby entity

---
+++ src/Entity/Lobby.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\LobbyRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=LobbyRepository::class)
+ */
+class Lobby
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $player_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPlayerId(): ?int
+    {
+        return $this->player_id;
+    }
+
+    public function setPlayerId(int $player_id): self
+    {
+        $this->player_id = $player_id;
+
+        return $this;
+    }
+}
